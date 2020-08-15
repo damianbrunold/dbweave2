54577c46b9059c1696985d5fc06cbe7f03cc1763
Damian Brunold <damian.brunold@bluewin.ch>
2020-08-15 22:26:07 +0200

    Add pattern, arrange components, better drawing
    We now arrange the components in the classical
    layout. The pattern is drawn on the fly. The view
    handles mouse clicks, but calls back into the
    controller for handling the event, changing the
    model and updating the views. Painter classes
    abstract a variety of drawing styles for the
    cells.

--- src/controller.kt
+++ src/controller.kt
@@ -1,74 +1,109 @@
 import javax.swing.JFrame
 import java.awt.EventQueue
-import java.awt.GridLayout
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
-import javax.swing.BorderFactory
-import javax.swing.JLabel
+import java.awt.Rectangle
+import java.awt.event.*
+
+interface UICallback {
+    fun toggleThreading(i: Int, j: Int)
+    fun toggleTieup(i: Int, j: Int)
+    fun toggleTreadling(i: Int, j: Int)
+    fun togglePattern(i: Int, j: Int)
+}
 
 class Dbweave(title: String) : JFrame() {
+
+    val settings = ViewSettings()
+
+    val model = Model(500, 500)
+
+    init {
+        for (i in 0 until model.threading.size) {
+            model.threading[i, i % 8] = true
+        }
+        for (j in 0 until model.treadling.size) {
+            model.treadling[j % 8, j] = true
+        }
+        for (i in 0 until 8) {
+            model.tieup[i, i] = true
+            model.tieup[i, (i + 1) % 8] = true
+            model.tieup[i, (i + 2) % 8] = true
+        }
+    }
+
+    val callback: UICallback = object : UICallback {
+        override fun toggleThreading(i: Int, j: Int) {
+            model.threading[i, j] = model.threading[i] != j
+            threadingView.invalidate()
+            threadingView.repaint()
+            patternView.invalidate()
+            patternView.repaint()
+        }
+
+        override fun toggleTieup(i: Int, j: Int) {
+            model.tieup[i, j] = !model.tieup[i, j]
+            tieupView.invalidate()
+            tieupView.repaint()
+            patternView.invalidate()
+            patternView.repaint()
+        }
+
+        override fun toggleTreadling(i: Int, j: Int) {
+            model.treadling[i, j] = !model.treadling[i, j]
+            treadlingView.invalidate()
+            treadlingView.repaint()
+            patternView.invalidate()
+            patternView.repaint()
+        }
+
+        override fun togglePattern(i: Int, j: Int) {
+            // TODO
+        }
+    }
+
+    val threadingView = ThreadingView(model.threading, callback, settings, VerticalPainter())
+    val tieupView = TieupView(model.tieup, callback, settings, CrossPainter())
+    val treadlingView = TreadlingView(model.treadling, callback, settings, DotPainter())
+    val patternView = PatternView(model, callback, settings, FillPainter())
 
     init {
         createUI(title)
     }
 
     private fun createUI(title: String) {
-
         setTitle(title)
 
         defaultCloseOperation = EXIT_ON_CLOSE
         setSize(1024, 768)
         setLocationRelativeTo(null)
 
-        layout = GridLayout(2, 2)
+        layout = null
 
-        val settings = ViewSettings()
-
-        val model = Model(50, 50)
-
-        val threadingView = ThreadingView(model.threading, settings)
         add(threadingView)
-
-        val tieupView = TieupView(model.tieup, settings)
         add(tieupView)
-
-        val patternView = JLabel("Pattern")
         add(patternView)
-
-        val treadlingView = TreadlingView(model.treadling, settings)
         add(treadlingView)
 
-        threadingView.addMouseListener(object : MouseAdapter() {
-            override fun mouseReleased(e: MouseEvent) {
-                val i = e.x / settings.dx
-                val j = (threadingView.height - e.y) / settings.dy
-                if (model.threading[i] == j) model.threading[i, j] = false
-                else model.threading[i, j] = true
-                threadingView.invalidate()
-                threadingView.repaint()
-            }
+        arrangeComponents()
+
+        addWindowListener(object : WindowAdapter() {
+            // TODO
         })
 
-        treadlingView.addMouseListener(object : MouseAdapter() {
-            override fun mouseReleased(e: MouseEvent) {
-                val i = e.x / settings.dx
-                val j = (treadlingView.height - e.y) / settings.dy
-                model.treadling[i, j] = !model.treadling[i, j]
-                treadlingView.invalidate()
-                treadlingView.repaint()
+        addComponentListener(object : ComponentAdapter() {
+            override fun componentResized(e: ComponentEvent?) {
+                super.componentResized(e)
+                arrangeComponents()
             }
         })
+    }
 
-        tieupView.addMouseListener(object : MouseAdapter() {
-            override fun mouseReleased(e: MouseEvent) {
-                val i = e.x / settings.dx
-                val j = (tieupView.height - e.y) / settings.dy
-                model.tieup[i, j] = !model.tieup[i, j]
-                tieupView.invalidate()
-                tieupView.repaint()
-            }
-        })
-
+    private fun arrangeComponents() {
+        val xx = width - (settings.treadling_visible + 1) * settings.dx
+        val yy = (settings.threading_visible + 1) * settings.dy
+        threadingView.bounds = Rectangle(0, 0, xx - settings.dx + 1, settings.threading_visible * settings.dy + 1)
+        tieupView.bounds = Rectangle(xx, 0, settings.treadling_visible * settings.dx + 1, settings.threading_visible * settings.dy + 1)
+        treadlingView.bounds = Rectangle(xx, yy, settings.treadling_visible * settings.dx + 1, height - yy - settings.dy + 1)
+        patternView.bounds = Rectangle(0, yy, xx - settings.dx + 1, height - yy - settings.dy + 1)
     }
 }
 
--- src/view.kt
+++ src/view.kt
@@ -1,5 +1,8 @@
 import java.awt.Color
 import java.awt.Graphics
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.BorderFactory
 import javax.swing.JComponent
 
 class ViewSettings {
@@ -9,7 +12,51 @@ class ViewSettings {
     var treadling_visible = 12
 }
 
-class ThreadingView(val data: SingleGrid, val settings: ViewSettings): JComponent() {
+interface Painter {
+    fun paintCell(p0: Graphics, x: Int, y: Int, width: Int, height: Int)
+}
+
+class FillPainter : Painter {
+    override fun paintCell(p0: Graphics, x: Int, y: Int, width: Int, height: Int) {
+        p0.fillRect(x + 2, y + 2, width - 3, height - 3)
+    }
+}
+
+class CrossPainter : Painter {
+    override fun paintCell(p0: Graphics, x: Int, y: Int, width: Int, height: Int) {
+        p0.drawLine(x + 2, y + 2, x + width - 2, y + height - 2)
+        p0.drawLine(x + 2, y + height - 2, x + width - 2, y + 2)
+    }
+}
+
+class VerticalPainter : Painter {
+    override fun paintCell(p0: Graphics, x: Int, y: Int, width: Int, height: Int) {
+        p0.drawLine(x + width / 2 - 1, y + 2, x + width / 2 - 1, y + height - 2)
+        p0.drawLine(x + width / 2, y + 2, x + width / 2, y + height - 2)
+        p0.drawLine(x + width / 2 + 1, y + 2, x + width / 2 + 1, y + height - 2)
+    }
+}
+
+class DotPainter : Painter {
+    override fun paintCell(p0: Graphics, x: Int, y: Int, width: Int, height: Int) {
+        p0.drawLine(x + width / 2 - 1, y + height / 2 - 1, x + width / 2 + 1, y + height / 2 - 1)
+        p0.drawLine(x + width / 2 - 1, y + height / 2 + 0, x + width / 2 + 1, y + height / 2 + 0)
+        p0.drawLine(x + width / 2 - 1, y + height / 2 + 1, x + width / 2 + 1, y + height / 2 + 1)
+    }
+}
+
+class ThreadingView(val data: SingleGrid, val callback: UICallback, val settings: ViewSettings, val painter: Painter): JComponent() {
+    init {
+        border = BorderFactory.createLineBorder(Color.BLACK)
+        addMouseListener(object : MouseAdapter() {
+            override fun mouseReleased(e: MouseEvent) {
+                val i = e.x / settings.dx
+                val j = (height - e.y) / settings.dy
+                callback.toggleThreading(i, j)
+            }
+        })
+    }
+
     override fun paintComponent(p0: Graphics) {
         val dx = settings.dx
         val dy = settings.dy
@@ -21,53 +68,116 @@ class ThreadingView(val data: SingleGrid, val settings: ViewSettings): JComponen
         for (j in 0..threading_visible) {
             p0.drawLine(0, height - 1 - j * dy, data.size * dx, height - 1 - j * dy)
         }
+        p0.color = Color.BLACK
         for (i in 0 until data.size) {
             val j = data[i]
             if (j == -1) continue
-            p0.fillRect(i * dx, height - 1 - (j + 1) * dy, dx, dy)
+            painter.paintCell(p0, i * dx, height - 1 - (j + 1) * dy, dx, dy)
         }
     }
 }
 
-class TreadlingView(val data: Grid, val settings: ViewSettings): JComponent() {
+class TreadlingView(val data: Grid, val callback: UICallback, val settings: ViewSettings, val painter: Painter): JComponent() {
+    init {
+        border = BorderFactory.createLineBorder(Color.BLACK)
+        addMouseListener(object : MouseAdapter() {
+            override fun mouseReleased(e: MouseEvent) {
+                val i = e.x / settings.dx
+                val j = (height - e.y) / settings.dy
+                callback.toggleTreadling(i, j)
+            }
+        })
+    }
+
     override fun paintComponent(p0: Graphics) {
         val dx = settings.dx
         val dy = settings.dy
         val treadling_visible = settings.treadling_visible
-        p0.color = Color.GREEN
+        p0.color = Color.BLACK
         for (i in 0..treadling_visible) {
             p0.drawLine(i * dx, height - 1 - 0, i * dx, height - 1 - data.size * dy)
         }
         for (j in 0..data.size) {
             p0.drawLine(0, height - 1 - j * dy, treadling_visible * dx, height - 1 - j * dy)
         }
+        p0.color = Color.BLACK
         for (i in 0 until treadling_visible) {
             for (j in 0 until data.size) {
                 if (data[i, j]) {
-                    p0.fillRect(i * dx, height - 1 - (j + 1) * dy, dx, dy)
+                    painter.paintCell(p0, i * dx, height - 1 - (j + 1) * dy, dx, dy)
                 }
             }
         }
     }
 }
 
-class TieupView(val data: Grid, val settings: ViewSettings): JComponent() {
+class TieupView(val data: Grid, val callback: UICallback, val settings: ViewSettings, val painter: Painter): JComponent() {
+    init {
+        border = BorderFactory.createLineBorder(Color.BLACK)
+        addMouseListener(object : MouseAdapter() {
+            override fun mouseReleased(e: MouseEvent) {
+                val i = e.x / settings.dx
+                val j = (height - e.y) / settings.dy
+                callback.toggleTieup(i, j)
+            }
+        })
+    }
+
     override fun paintComponent(p0: Graphics) {
         val dx = settings.dx
         val dy = settings.dy
         val threading_visible = settings.threading_visible
         val treadling_visible = settings.treadling_visible
-        p0.color = Color.RED
+        p0.color = Color.BLACK
         for (i in 0..treadling_visible) {
             p0.drawLine(i * dx, height - 1 - 0, i * dx, height - 1 - threading_visible * dy)
         }
         for (i in 0..threading_visible) {
             p0.drawLine(0, height - 1 - i * dy, treadling_visible * dx, height - 1 - i * dy)
         }
+        p0.color = Color.BLACK
         for (i in 0 until treadling_visible) {
             for (j in 0 until threading_visible) {
                 if (data[i, j]) {
-                    p0.fillRect(i * dx, height - 1 - (j + 1) * dy, dx, dy)
+                    painter.paintCell(p0, i * dx, height - 1 - (j + 1) * dy, dx, dy)
+                }
+            }
+        }
+    }
+}
+
+class PatternView(val model: Model, val callback: UICallback, val settings: ViewSettings, val painter: Painter): JComponent() {
+    init {
+        border = BorderFactory.createLineBorder(Color.BLACK)
+        addMouseListener(object : MouseAdapter() {
+            override fun mouseReleased(e: MouseEvent) {
+                val i = e.x / settings.dx
+                val j = (height - e.y) / settings.dy
+                callback.togglePattern(i, j)
+            }
+        })
+    }
+
+    override fun paintComponent(p0: Graphics) {
+        val dx = settings.dx
+        val dy = settings.dy
+        p0.color = Color.BLACK
+        for (i in 0..model.threading.size) {
+            p0.drawLine(i * dx, height - 1 - 0, i * dx, height - 1 -  model.treadling.size * dy)
+        }
+        for (j in 0..model.treadling.size) {
+            p0.drawLine(0, height - 1 - j * dy, model.threading.size * dx, height - 1 - j * dy)
+        }
+        for (i in 0 until model.threading.size) {
+            for (j in 0 until model.treadling.size) {
+                val threading = model.threading[i]
+                if (threading == -1) continue
+                for (m in 0 until model.treadling.size) {
+                    if (!model.treadling[m, j]) continue
+                    if (model.tieup[m, threading]) {
+                        painter.paintCell(p0, i * dx, height - 1 - (j + 1) * dy, dx, dy)
+                        break
+                    }
                 }
             }
         }
